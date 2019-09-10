d7552f3bfb25709b7959ba61168ea5aadd1354cc
katie <katelynneuhaus96@hotmail.com>
2019-09-10 02:27:12 -0500

    Phphomework

---
+++ PHP Basics.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>PHP Basics</title>
+</head>
+
+<body>
+    <script> document.write("<h1> PHP Basics </h1>");</script>
+    <h2>Katelyn Neuhaus</h2>
+
+    <p>Number 1: 1 <p> Number 2: 2 <p> 1 + 2 = 3</p>
+  
+	
+</body>
+</html>
+
